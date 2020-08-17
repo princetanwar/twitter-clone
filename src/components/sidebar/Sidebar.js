55157121ba864877ee0296816c92500ddd245675
prince tanwar <ptanwar468@gmail.com>
2020-08-17 09:28:22 +0530

    sidebar added

---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SidebarOption from "../sidebarOption/SidebarOption";
+import { Button } from "@material-ui/core";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      {/* Twitter icon */}
+      <TwitterIcon />
+
+      {/* Sidebar Options */}
+      <SidebarOption active="active" text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Explore" Icon={SearchIcon} />
+      <SidebarOption text="Notifications" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="Messages" Icon={MailOutlineIcon} />
+      <SidebarOption text="Bookmark" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="Lists" Icon={ListAltIcon} />
+      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+      <SidebarOption text="More" Icon={MoreHorizIcon} />
+
+      {/* Button -> Tweet */}
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/sidebarOption/SidebarOption.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sidebarOption.css";
+
+const SidebarOption = ({ active, text, Icon }) => {
+  return (
+    <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+      <Icon />
+      <h2>{text}</h2>
+    </div>
+  );
+};
+
+export default SidebarOption;
